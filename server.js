10a3ddf27b600c315dc37a52a487d089f4dadd92
Vadim Volotskoy <versusvirus@Mail.ru>
2018-02-26 22:47:44 +0300

    Инициализация проекта

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-var express =
+var express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http);
+
+app.use(express.static('src'));
+app.use(express.static('templates'));
+
+app.get('/', function (req, res) {
+    res.sendFile('/registry.html');
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
+
+
+
